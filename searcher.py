c1943cc0b9d9ba2d84259f6c41b54f600a87536f
refathex <khondakar00@gmail.com>
2024-05-02 14:49:26 +0600

    added needed packages iin env

--- searcher.py
+++ searcher.py
@@ -1,25 +1,2 @@
-import requests
-def call_gemini_api_with_search(search_prompt):
-    url = "https://api.gemini.com/v1/some_endpoint" 
-    
-    headers = {
-        "Content-Type": "application/json",
-        "Authorization": "Bearer YOUR_API_KEY"
-    }
-    params = {
-        "param1": "value1",
-        "param2": "value2",
-        "search": search_prompt
-    }
-    
-    # Send the GET request with stream=True to enable streaming
-    response = requests.get(url, headers=headers, params=params, stream=True)
-    
-    # Process the response stream
-    for chunk in response.iter_content(chunk_size=1024):
-        if chunk:
-            # Do something with the chunk of data
-            print(chunk)
-    
-    # Close the response stream
-    response.close()
+import google.generativeai as gen_ai
+import streamlit as st
